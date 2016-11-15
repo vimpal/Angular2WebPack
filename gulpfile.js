ebaa41bff4ec959c345bf0b67d7211ab62f1a883
vimpal <vimpal.singh@gmail.com>
2016-11-15 17:08:21 +0530

    Update gulpfile.js

--- gulpfile.js
+++ gulpfile.js
@@ -7,6 +7,7 @@ var jasmine = require('gulp-jasmine');
 var join = require('path').join;
 var reporters   = require('jasmine-reporters');
 var ts = require('gulp-typescript');
+const del = require('del');
 
 gulp.task('clean', function () {
   return del('dist/**/*');
